767766973aeef5547693145c1998a53cd5eec3cf
austinobioma <85629383+austinobioma@users.noreply.github.com>
2021-08-02 17:17:40 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,3 @@
 hello world,
 this is my first github commit. i will be an expert someday and i will eventually make money working as an IT personal by God's grace and my commitment
-
+this is an interesting build
